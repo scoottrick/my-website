2141d3b60be9ef885bf130d2b267698e7f47a824
scoottrick <csschapker94@gmail.com>
2024-06-11 00:54:50 -0500

    Removes unused styles

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -10,7 +10,6 @@ const portfolio = props.portfolio;
 <template>
     <div class="portfolio">
         <section v-for="section in portfolio" :key="section.id">
-            <h1>{{ section.title }}</h1>
             <ul class="project-list">
                 <li v-for="project in section.projects" :key="project.id" class="project-container">
                     <ProjectView :project="project" />
@@ -26,41 +25,11 @@ const portfolio = props.portfolio;
     padding-bottom: 20vh;
 }
 section:not(:last-child) {
-    margin-bottom: 4em;
+    margin-bottom: 8rem;
 }
-section h1 {
-    line-height: 4;
-}
-section p {
-    line-height: 1.2;
-}
-ul {
-    padding: 0;
-    list-style: none;
-    margin: 0 auto;
-}
-
 .project-list {
-    --direction: column;
-    gap: 4rem;
     display: flex;
-    flex-direction: var(--direction);
-    flex-wrap: wrap;
-}
-
-.project-list h2,
-.project-list p:not(:last-child) {
-    margin-bottom: 1em;
-}
-
-@media screen and (min-width: 1680px) {
-    .project-list {
-        /* --direction: row; */
-    }
-    .project-container {
-        flex-grow: 1;
-        flex-shrink: 1;
-        flex-basis: 45%;
-    }
+    flex-direction: column;
+    gap: 8rem;
 }
 </style>
