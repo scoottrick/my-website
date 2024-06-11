64ed70cbb9b4516bd71282f6e5178e4a7ce453d3
scoottrick <csschapker94@gmail.com>
2024-06-10 22:17:58 -0500

    Adds basic image gallery

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -6,28 +6,98 @@ const { cover } = defineProps({
 });
 
 const isOpened = ref(false);
+const current = ref(0);
 const tracker = ref(null);
+const closePrompt = ref(null);
+const coverImage = ref(null);
+const galleryView = ref(null);
 const containerClass = computed(() => `image-viewer ${isOpened.value ? 'opened' : 'closed'}`);
+const galleryImages = [];
+for (let i = 0; i < 10; i++) {
+    galleryImages.push(cover);
+}
 
-function trackMouse(event) {
+function openGallery() {
+    current.value = 0;
+    isOpened.value = true;
+    document.body.classList.add('noscroll');
+}
+
+function closeGallery() {
+    isOpened.value = false;
+    document.body.classList.remove('noscroll');
+}
+
+function goToPrevious(event) {
+    event.stopPropagation();
+    if (current.value === 0) {
+        current.value = galleryImages.length - 1;
+    } else {
+        current.value--;
+    }
+}
+
+function goToNext(event) {
+    event.stopPropagation();
+    if (current.value === galleryImages.length - 1) {
+        current.value = 0;
+    } else {
+        current.value++;
+    }
+}
+
+function movePrompt(prompt, x, y) {
     const offsetX = Math.floor(tracker.value.clientWidth / 2);
     const offsetY = Math.floor(tracker.value.clientHeight / 2);
-    const x = event.clientX - offsetX;
-    const y = event.clientY - offsetY;
-    tracker.value.animate([{ left: `${x}px`, top: `${y}px` }], { duration: 50, fill: 'forwards' });
+    const left = `${x - offsetX}px`;
+    const top = `${y - offsetY}px`;
+    prompt.animate([{ left, top }], { duration: 500, fill: 'forwards' });
+}
+
+function trackMouse(event) {
+    if (isOpened.value) {
+        return;
+    }
+    movePrompt(tracker.value, event.clientX, event.clientY);
+}
+
+function moveClosePrompt(event) {
+    if (!isOpened.value) {
+        return;
+    }
+    movePrompt(closePrompt.value, event.clientX, event.clientY);
 }
 </script>
 <template>
     <div :class="containerClass">
-        <img
-            class="cover"
-            @click="() => (isOpened = !isOpened)"
-            @mousemove="trackMouse"
-            :src="cover"
-            alt="project thumbnail"
-        />
-        <span class="prompt still"><i class="iconoir-enlarge"></i></span>
-        <span ref="tracker" class="prompt follow"><i class="iconoir-enlarge"></i></span>
+        <div class="cover">
+            <img
+                ref="coverImage"
+                @click="openGallery"
+                @mousemove="trackMouse"
+                :src="cover"
+                alt="project thumbnail"
+            />
+            <span class="prompt still"><i class="iconoir-enlarge"></i></span>
+            <span ref="tracker" class="prompt follow light"><i class="iconoir-enlarge"></i></span>
+        </div>
+        <div class="gallery" @mousemove="moveClosePrompt" @click="closeGallery">
+            <div class="interaction" @click.stop>
+                <img :src="galleryImages[current]" alt="" />
+                <div>
+                    <button @click.stop="goToPrevious">
+                        <i class="iconoir-arrow-left"></i>
+                    </button>
+                    <button @click.stop="goToNext"><i class="iconoir-arrow-right"></i></button>
+                </div>
+            </div>
+            <!-- <ul ref="galleryView" @click="cancelClick">
+                <li v-for="(image, i) in galleryImages" :key="i">
+                    <img :src="image" alt="" />
+                    </li>
+                    </ul> -->
+            <span ref="closePrompt" class="prompt light"><i class="iconoir-reduce"></i></span>
+        </div>
     </div>
 </template>
 
@@ -35,14 +105,72 @@ function trackMouse(event) {
 .image-viewer {
     position: relative;
 }
-img.cover {
+.image-viewer.closed .gallery {
+    display: none;
+}
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
+    background: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(2px);
+}
+.image-viewer.closed .overlay {
+    display: none;
+}
+.gallery {
+    background: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(2px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    padding: 2em;
+    cursor: pointer;
+}
+.gallery ul {
+    display: flex;
+    flex-direction: row;
+    overflow-x: scroll;
+    list-style: none;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    gap: 2em;
+}
+.interaction {
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+    align-items: center;
+}
+.gallery ul {
+    cursor: auto;
+}
+.gallery .interaction:hover ~ .prompt {
+    opacity: 0;
+}
+.gallery .prompt {
+    position: fixed;
+    opacity: 1;
+}
+.gallery img {
+    min-width: 400px;
+    min-height: 400px;
+}
+.cover img {
     position: relative;
     display: inline-block;
     box-shadow: none;
     transition: all 200ms ease-in-out;
-}
-.image-viewer.opened .prompt {
-    display: none;
 }
 
 .prompt {
@@ -70,9 +198,12 @@ img.cover {
     height: 60%;
     color: inherit;
 }
-.follow {
+
+.prompt.light {
     --background-color: white;
     --color: black;
+}
+.follow {
     position: fixed;
     opacity: 0;
 }
@@ -83,13 +214,13 @@ img.cover {
     bottom: 20px;
     right: 20px;
 }
-.image-viewer.closed img.cover:hover ~ .follow {
+.image-viewer.closed .cover img:hover ~ .follow {
     opacity: 1;
 }
-.image-viewer.closed img.cover:hover ~ .still {
+.image-viewer.closed .cover img:hover ~ .still {
     opacity: 0;
 }
-img.cover:hover {
+.cover img:hover {
     box-shadow: 2px 4px 8px -2px black;
     transform: scale(1.05);
 }
